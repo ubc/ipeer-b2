b9535c507bd9ef2cd47509c206ddac83df413d88
Compass <pan.luo@ubc.ca>
2013-01-28 22:44:32 -0800

    Remove in-memory cache from configuration
    In-memory cache may cause issue when multiple BB learn nodes are
    running.

--- src/ca/ubc/ctlt/ipeerb2/Configuration.java
+++ src/ca/ubc/ctlt/ipeerb2/Configuration.java
@@ -3,13 +3,9 @@ package ca.ubc.ctlt.ipeerb2;
 import java.io.IOException;
 import java.util.Properties;
 
-import javax.servlet.ServletContext;
-
-import org.springframework.web.context.ServletContextAware;
-
 import blackboard.platform.plugin.PlugInException;
 
-public class Configuration implements ServletContextAware {
+public class Configuration {
 	public static final String COURSE_ID = "mapping.course_id.";
 	public static final String IPEER_URL = "ipeer.url";
 	public static final String CONSUMER_KEY = "ipeer.consumer_key";
@@ -17,54 +13,31 @@ public class Configuration implements ServletContextAware {
 	public static final String TOKEN_KEY = "ipeer.token_key";
 	public static final String TOKEN_SECRET = "ipeer.token_secret";
 	public static final String ROLE_MAPPING_PREFIX = "ipeer.role_mapping";
-	
-	private ServletContext servletContext;
-	private boolean inMemory = false;
-	
-	public boolean isInMemory() {
-		return inMemory;
-	}
 
-	public void setInMemory(boolean inMemory) {
-		this.inMemory = inMemory;
-	}
-
-	public void setSettings(Properties settings) {
-		servletContext.setAttribute("settings", settings);
-	}
-	
-	@Override
-	public void setServletContext(ServletContext context) {
-		this.servletContext = context;
-	}
-	
 	public Properties getSettings() {
-		Properties settings = (Properties)servletContext.getAttribute("settings");
-		if (null == settings && !inMemory) {
-			try {
-				settings = BuildingBlockHelper.loadBuildingBlockSettings();
-				servletContext.setAttribute("settings", settings);
-			} catch (PlugInException e) {
-				throw new RuntimeException("There is an error loading the settings!", e);
-			} catch (IOException e) {
-				throw new RuntimeException("Could not read setting files!", e);
-			}
-		} else if (null == settings) {
-			settings = new Properties();
+		Properties settings = null;
+		try {
+			settings = BuildingBlockHelper.loadBuildingBlockSettings();
+			System.out.println("Loaded settings from file.");
+		} catch (PlugInException e) {
+			throw new RuntimeException(
+					"There is an error loading the settings!", e);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not read setting files!", e);
 		}
 		
+		System.out.println("Settings: " + settings);
 		return settings;
 	}
 	
-	public void saveSettings() {
-		if (!inMemory) {
-			try {
-				BuildingBlockHelper.saveBuildingBlockSettings(getSettings());
-			} catch (PlugInException e) {
-				throw new RuntimeException("There is an error loading the settings!", e);
-			} catch (IOException e) {
-				throw new RuntimeException("Could not write setting files!", e);
-			}
+	public void saveSettings(Properties settings) {
+		try {
+			BuildingBlockHelper.saveBuildingBlockSettings(settings);
+		} catch (PlugInException e) {
+			throw new RuntimeException(
+					"There is an error loading the settings!", e);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not write setting files!", e);
 		}
 	}
 	
@@ -77,13 +50,15 @@ public class Configuration implements ServletContextAware {
 	}
 	
 	public void setSetting(String key, String value) {
-		getSettings().setProperty(key, value);
-		saveSettings();
+		Properties settings = getSettings();
+		settings.setProperty(key, value);
+		saveSettings(settings);
 	}
 	
 	public void deleteSetting(String key) {
-		getSettings().remove(key);
-		saveSettings();
+		Properties settings = getSettings();
+		settings.remove(key);
+		saveSettings(settings);
 	}
 
 	public int getIpeerCourseId(String bbCourseId) {
@@ -104,6 +79,9 @@ public class Configuration implements ServletContextAware {
 	}
 	
 	public void setConnection(String bbCourseId, int ipeerCourseId) {
+		if (bbCourseId == null || "".equals(bbCourseId)) {
+			throw new RuntimeException("Invalid bbCourseId! Please refresh the page and try again.");
+		}
 		setSetting(COURSE_ID + bbCourseId, Integer.toString(ipeerCourseId));
 	}
 	
